3aa0a297058ae3d0d62f37d540247e441fbbfc09
marcospereiraribeiro <160537989+marcospereiraribeiro@users.noreply.github.com>
2024-11-06 20:30:48 -0300

    SEÇÃO 8 11:30MIN

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'Programação',
+    'O que é Java Script?',
+    'O Java Script é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a Capital da França?',
+    'A Capital da França é Paris.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
+)
