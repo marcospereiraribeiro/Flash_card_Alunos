30972b8811f635197bf0099ea9466a6873a2b2e3
marcospereiraribeiro <160537989+marcospereiraribeiro@users.noreply.github.com>
2024-11-06 22:15:13 -0300

    SEÇÃO 8 11:30MIN

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criaCartao(categoia, pergunta, resposta) {
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,43 @@
 criaCartao(
-    'Programação',
-    'O que é Java Script?',
-    'O Java Script é uma linguagem de programação.'
+    'Filosofia',
+    'De quem é a famosa frase "Penso, logo existo"?',
+    'A famosa frase "Penso, logo existo" é do folosófo René Descartes.'
 )
 
 criaCartao(
     'Geografia',
     'Qual é a Capital da França?',
     'A Capital da França é Paris.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco que executa alguma tarefa.'
 )
 criaCartao(
     'Programação',
